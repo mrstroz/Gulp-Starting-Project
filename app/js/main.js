c0d615625867eb2d2fc42cec440c1754119ac07b
Paweł Chyła <chyla1988@gmail.com>
2017-12-11 19:03:39 +0100

    Change module to iife in js

--- app/js/main.js
+++ app/js/main.js
@@ -1,18 +1,12 @@
-var App = function () {
-
+(() => {
     const test = () => {
-        console.log(test);
-    };
-
-    function init () {
-        test();
+      console.log();
+    }
+    const init = () => {
+      test();
     }
 
-    return {
-        init: init
-    }
-}();
-
-document.addEventListener('DOMContentLoaded', function() {
-    App.init();
-});
+    document.addEventListener('DOMContentLoaded', () => {
+      init();
+    });
+  })()
